a297fce9988920a6d99dc3499cc39fdf64684aaa
Zeroxytb06 <105605589+Zeroxytb06@users.noreply.github.com>
2022-07-16 20:39:02 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+Its a python file for unpackage executable file to pyd file Dev By Zerox#8908
+Print __init__.py and slide your file and decompyle
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+ name='zextractror',
+ version='0.1',
+ author="Noam Sghaier Weil",
+ author_email="zerox.ytb06@gmail.com",
+ description="Its a python file for unpackage executable file to pyd file",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
